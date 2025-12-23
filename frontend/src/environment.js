52ee07da3fd049a29a61d0a41ceccf86fb3b2653
Pratik Kadam <pratikkadam4696@gmail.com>
2025-12-24 01:55:37 +0530

    ready frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,7 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+
+const server = IS_PROD
+  ? "https://apnacollegebackend.onrender.com"
+  : "http://localhost:8000";
+
 export default server;
